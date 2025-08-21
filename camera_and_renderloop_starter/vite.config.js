f8dfed3aa0d68bb23937cd9996b997675f1f542d
Pushkar Dixit <pushkard@heaptrace.com>
2025-08-22 02:45:41 +0530

    try to fix isssue7

--- camera_and_renderloop_starter/vite.config.js
+++ camera_and_renderloop_starter/vite.config.js
@@ -2,15 +2,26 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  root: '.',          // project root
-  publicDir: 'public',
-  base: '/',          // good for Vercel/Netlify
+  root: '.',           // keep your root as current folder
+  publicDir: 'public', // where to put static files
+  base: './',          // ✅ important for correct asset paths in subpages
+
   build: {
     rollupOptions: {
       input: {
+        // index
         main: resolve(__dirname, 'index.html'),
-        transform: resolve(__dirname, 'Controls/TransformControls.html'),
+
+        // controls pages
+        orbit: resolve(__dirname, 'Controls/orbit.html'),
+        arcball: resolve(__dirname, 'Controls/ArcballControls.html'),
+        drag: resolve(__dirname, 'Controls/DragControls.html'),
         firstPerson: resolve(__dirname, 'Controls/FirstPersonControls.html'),
+        fly: resolve(__dirname, 'Controls/FlyControls.html'),
+        map: resolve(__dirname, 'Controls/MapControls.html'),
+        pointer: resolve(__dirname, 'Controls/poiner.html'),
+        trackball: resolve(__dirname, 'Controls/TrackballControls.html'),
+        transform: resolve(__dirname, 'Controls/TransformControls.html'),
       },
     },
   },
