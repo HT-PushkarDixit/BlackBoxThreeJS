484c8815084ad7e380f2ca5641fcbf725cb1cbad
Pushkar Dixit <pushkard@heaptrace.com>
2025-08-22 02:28:46 +0530

    try to fix isssue3

--- camera_and_renderloop_starter/vite.config.js
+++ camera_and_renderloop_starter/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  root: '.',                 
+  root: '.',                
   publicDir: 'public',       
   base: '/',                 
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        transform: resolve(__dirname, 'Controls/TransformControls.html'),
-        firstPerson: resolve(__dirname, 'Controls/FirstPersonControls.html'),
+        transform: resolve(__dirname, 'src/Controls/TransformControls.html'),
+        firstPerson: resolve(__dirname, 'src/Controls/FirstPersonControls.html'),
       },
     },
   },
-});
+})
