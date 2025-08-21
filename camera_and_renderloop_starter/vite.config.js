714c14f9505eb8cecdaf94030bd2ec4f2f1adceb
Pushkar Dixit <pushkard@heaptrace.com>
2025-08-22 02:36:15 +0530

    try to fix isssue6

--- camera_and_renderloop_starter/vite.config.js
+++ camera_and_renderloop_starter/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  root: '.',                
-  publicDir: 'public',       
-  base: '/',                 
+  root: '.',          // project root
+  publicDir: 'public',
+  base: '/',          // good for Vercel/Netlify
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        transform: resolve(__dirname, 'src/Controls/TransformControls.html'),
-        firstPerson: resolve(__dirname, 'src/Controls/FirstPersonControls.html'),
+        transform: resolve(__dirname, 'Controls/TransformControls.html'),
+        firstPerson: resolve(__dirname, 'Controls/FirstPersonControls.html'),
       },
     },
   },
